52d7a332e1c0e6bb340ed2a4cc7f85793f9bbc05
chaeyeonnnn <138082742+chaeyeonnnn@users.noreply.github.com>
2023-11-15 11:34:50 +0900

    Update parse.cpp

--- parse.cpp
+++ parse.cpp
@@ -82,8 +82,42 @@ private:
     
     pcap_t *handle;
     void Parse(pcap_pkthdr* header,const u_char *data, uint caplen);
-    void parsebody(const u_char *payload, int dataLength);
+    void parseEthernet(const Ether& eth);
+    void parseIP(const IP& ip);
+    void parseTCP(const TCP& tcp, int dataLength);
+    void parsejson(const u_char* payload, int dataLength);
 };
+
+void PcapParser::Parse(pcap_pkthdr* header, const u_char *data, uint caplen) {
+    
+    Packet* packet = (Packet *)data;
+    ushort ethertype = ntohs(packet->eth.ether_type); 
+
+    int ethernetHeaderLength = sizeof(Ether);
+    uchar pro_id = packet->ip.protocol_id;
+    ushort totallen = ntohs(packet->ip.total_len);
+    uchar ip_header_len = (packet->ip.version & 0x0F) * 4;
+    uchar tcp_header_len = (packet->tcp.offsets >> 4) * 4;
+    ushort srcPort = ntohs(packet->tcp.src_port);
+    ushort dstPort = ntohs(packet->tcp.dst_port);
+    int dataLength = caplen - ethernetHeaderLength - ip_header_len - tcp_header_len;
+
+
+    if (ethertype == 0x0800&&pro_id == 6 &&(srcPort == 8080 || dstPort==8080) && (dataLength>0)){
+        parseEthernet(packet->eth);
+        int ethernetHeaderLength = sizeof(Ether);
+        const u_char* ipHeaderStart = data + ethernetHeaderLength;
+
+        parseIP(packet->ip);
+        parseTCP(packet->tcp, caplen-ethernetHeaderLength-(packet->ip.version & 0x0F) * 4 - (packet->tcp.offsets >> 4) * 4);
+        const u_char* datastart = ipHeaderStart + sizeof(IP) + sizeof(TCP);
+        memcpy(packet->payload, datastart, dataLength);
+        parsejson(datastart + 1, dataLength);
+
+    }
+
+}
+
 
 PcapParser::PcapParser(const string &file)
 {
@@ -106,136 +140,94 @@ PcapParser::~PcapParser()
     }
 }
 
+void PcapParser::parseEthernet(const Ether& eth){
+    cout << "--------Ethernet frame-----------" << endl;
+    cout << "Ehter type: " << hex << setw(4) << setfill('0') << ntohs(eth.ether_type) << dec << endl;
 
-void PcapParser::Parse(pcap_pkthdr* header,const u_char *data, uint caplen)
-{
-    Packet* packet = (Packet *)data;
-
-    ushort etherType = ntohs(packet->eth.ether_type);
-
-    if (etherType == 0x0800){
-        
-        int ethernetHeaderLength = sizeof(Ether);
-        //cout << "size: " <<ethernetHeaderLength <<endl;
-        //14
-        const u_char *ipHeaderStart = data + ethernetHeaderLength;
-
-        ushort totallen = ntohs(packet->ip.total_len);
-        uchar ip_header_len = (packet->ip.version & 0x0F) * 4;
-        uchar tcp_header_len = (packet->tcp.offsets >> 4) * 4;
-
-        ushort srcPort = ntohs(packet->tcp.src_port);
-        ushort dstPort = ntohs(packet->tcp.dst_port);
-
-        uchar pro_id = packet->ip.protocol_id;
-
-        int dataLength = caplen - ethernetHeaderLength - ip_header_len - tcp_header_len;
-        if (pro_id == 6 &&(srcPort == 80 || dstPort==80) && (dataLength>0)){
-
-
-            cout << "-------------------new packet--------------------" << endl;
-            cout << "Timestamp: " << dec << header->ts.tv_sec << "." << header->ts.tv_usec << endl;
-            cout << "Capture Length: " << dec << header->caplen << " bytes" << endl;
-            
-            cout << "--------Ethernet frame-----------" << endl;
-            cout << "Ehter type: " << hex << setw(4) << setfill('0') << ntohs(packet->eth.ether_type) << dec << endl;
-
-            cout << "Src MAC: ";
-            for (int i = 0; i < 6; i++)
-            {
-                cout << hex << setw(2) << setfill('0') << int(packet->eth.SA[i]);
-                if (i < 5)
-                {
-                    cout << ":";
-                }
-            }
-            cout << dec << endl;
-
-            cout << "Dst MAC: ";
-            for (int i = 0; i < 6; i++)
-            {
-                cout << hex << setw(2) << setfill('0') << int(packet->eth.DA[i]);
-                // 왼쪽은 0으로 채운다
-                if (i < 5)
-                {
-                    cout << ":";
-                }
-            }
-            cout << dec << endl;
-
-            cout << "--------------IP------------------" << endl;
-            cout << "Protocol ID: " << hex << setw(2) << setfill('0') << int(pro_id) << dec << endl;
-            
-            //fixedip...???
-            uint32_t src_ip = packet->ip.src_ip; 
-            uint32_t dst_ip = packet->ip.dst_ip;
-
-            char src_ip_str[INET_ADDRSTRLEN];
-            char dst_ip_str[INET_ADDRSTRLEN]; 
-            
-            inet_ntop(AF_INET, &src_ip, src_ip_str, INET_ADDRSTRLEN);
-            inet_ntop(AF_INET, &dst_ip, dst_ip_str, INET_ADDRSTRLEN);
-
-            printf("src IP : %s\n", src_ip_str);
-            printf("dst IP : %s\n", dst_ip_str);
-
-            cout << "total length : " << totallen << endl;
-            cout << "IP header length : " << int(ip_header_len) << endl;
-            cout << "TCP header length : " << int(tcp_header_len) << endl;
-
-            cout << "------------------TCP--------------------" << endl;
-            cout << "Src port: " << srcPort << endl;
-            cout << "Dst port: " << dstPort << endl;
-
-            const u_char *datastart = ipHeaderStart + sizeof(IP) + sizeof(TCP);
-            memcpy(packet->payload, datastart+1, dataLength);
-
-            cout << "------------------------------------" << endl;
-            if (dataLength > 0)
-            {
-                cout << "Data:" << endl;
-                for (int i = 0; i < dataLength; ++i)
-                
-                ///r/n 이 .. 으로 나옴 —> 먼저 0d0a로 나눠서 그 이후에 바꾸기
-                /*
-                {
-                    
-                    char c = packet->payload[i];
-                    if (isprint(c))
-                    {
-                        cout << c;
-                    }
-                    else{
-                        cout << ".";
-                    }
-                    //
-
-                }
-                */
-               {
-                cout << hex << setw(2) << setfill('0') << static_cast<int>(packet->payload[i]) << " ";
-               }
-               
-            }
-            cout << "…………………………data end" << endl;    
+    cout << "Src MAC: ";
+    for (int i = 0; i < 6; i++)
+    {
+        cout << hex << setw(2) << setfill('0') << int(eth.SA[i]);
+        if (i < 5)
+        {
+            cout << ":";
         }
-        else{}
     }
-    else{}
+    cout << dec << endl;
+
+    cout << "Dst MAC: ";
+    for (int i = 0; i < 6; i++)
+    {
+        cout << hex << setw(2) << setfill('0') << int(eth.DA[i]);
+        // 왼쪽은 0으로 채운다
+        if (i < 5)
+        {
+            cout << ":";
+        }
+    }
+    cout << dec << endl;
 }
 
-/*
-//여기에서 http 메시지 파싱
-void PcapParser::parsebody(const u_char *payload, int dataLength)
-{
+void PcapParser::parseIP(const IP& ip){
 
-    for (int i=0; i<dataLength; ++i){
-        char c = payload[i];
-        if
-    } 
+    cout << "--------------IP------------------" << endl;
+    cout << "Protocol ID: " << hex << setw(2) << setfill('0') << int(ip.protocol_id) << dec << endl;
+    
+    //fixedip...???
+    uint32_t src_ip = ip.src_ip; 
+    uint32_t dst_ip = ip.dst_ip;
+
+    char src_ip_str[INET_ADDRSTRLEN];
+    char dst_ip_str[INET_ADDRSTRLEN]; 
+    
+    inet_ntop(AF_INET, &src_ip, src_ip_str, INET_ADDRSTRLEN);
+    inet_ntop(AF_INET, &dst_ip, dst_ip_str, INET_ADDRSTRLEN);
+
+    printf("src IP : %s\n", src_ip_str);
+    printf("dst IP : %s\n", dst_ip_str);
+
+    cout << "total length : " << int(ntohs(ip.total_len)) << endl;
+    cout << "IP header length : " << int(ip.version & 0x0F) * 4 << endl;
 
 }
-*/
+
+void PcapParser::parseTCP(const TCP& tcp, int dataLength){
+    cout << "------------------TCP--------------------" << endl;
+    cout << "Src port: " << ntohs(tcp.src_port) << endl;
+    cout << "Dst port: " << ntohs(tcp.dst_port) << endl;
+
+}
+
+void PcapParser::parsejson(const u_char* payload, int dataLength){
+    //content-type이 appication/json이면 파싱하돋록
+    cout << "------------------------------------" << endl;
+    if (dataLength > 0)
+    {
+        cout << "Data:" << endl;
+        for (int i = 0; i < dataLength; ++i)        
+        {
+            
+            char c = payload[i];
+            if (isprint(c))
+            {
+                cout << c;
+            }
+            else{
+                cout << ".";
+            }
+            //
+
+        }
+        /*
+        {
+        cout << hex << setw(2) << setfill('0') << static_cast<int>(packet->payload[i]) << " ";
+        }*/
+        
+    }
+    cout << "…………………………data end" << endl;
+}
+
+
 
 
 int PcapParser::ParsePcapFile()
@@ -250,7 +242,9 @@ int PcapParser::ParsePcapFile()
     }
     return 0;
 }
+
 /*
+//실시간
 int PcapParser::Capture(int capturetime)
 {
     struct pcap_pkthdr header;
@@ -278,18 +272,18 @@ int PcapParser::Capture(int capturetime)
     return 0;
 }*/
 
+
+
 int main()
 {
-    string file = "test_pcap.pcapng";
+    string file = "test3_pcqp.pcapng";
+    // content-type='application/json'있는거 
 
-    //string file = "test3.pcqp.pcapng";--> 포트 8080으로 바꿔서
-    //content-type='application/json'있는거 
-
+    //string file = "test1.pcap.pcapng";
+    //content-type='application/ocsp-request,,,'
     PcapParser pcapParser(file);
-    int result = pcapParser.ParsePcapFile();
-    return result;
+    pcapParser.ParsePcapFile();
 }
-
 
 
 
